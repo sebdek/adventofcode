57512f845700f27469d4002fbb1bb7697238d9e2
Sébastien DEKEYSER <sdekeyser@oppido.fr>
2019-12-05 17:55:52 +0100

    opti code

--- src/fr/sebdek/adventofcode/Day2Test1.java
+++ src/fr/sebdek/adventofcode/Day2Test1.java
@@ -6,37 +6,40 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day2Test1 {
 
     public static void main(String[] args) throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader("day2_test1.txt"))) {
             String intcode = reader.readLine();
-            String[] intcodeArray = intcode.split(",");
-            for (int i = 0; i < intcodeArray.length; i = i + 4) {
-                List<Integer> positionValueList = processOpcodeFragment(intcodeArray, Arrays.copyOfRange(intcodeArray, i, i + 4));
+            List<Integer> intcodeList = Arrays.stream(intcode.split(","))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            for (int i = 0; i < intcodeList.size(); i = i + 4) {
+                List<Integer> positionValueList = processOpcodeFragment(intcodeList, intcodeList.subList(i, i + 4) );
                 if (positionValueList.get(1) == -1) {
                     break;
                 } else {
-                    intcodeArray[positionValueList.get(0)] = String.valueOf(positionValueList.get(1));
+                    intcodeList.set(positionValueList.get(0), positionValueList.get(1));
                 }
             }
-            System.out.println(String.join(",", intcodeArray));
+            System.out.println(intcodeList);
         }
     }
 
-    private static List<Integer> processOpcodeFragment(String[] intcodeArray, String[] opcodeFragment) {
-        int opcode = Integer.parseInt(opcodeFragment[0]);
+    private static List<Integer> processOpcodeFragment(List<Integer> intcodeList, List<Integer> opcodeFragment) {
+        int opcode = opcodeFragment.get(0);
         int result = 0;
         switch (opcode) {
             case 1:
-                if (opcodeFragment[1] != null && opcodeFragment[2] != null) {
-                    result = Integer.parseInt(intcodeArray[Integer.parseInt(opcodeFragment[1])]) + Integer.parseInt(intcodeArray[Integer.parseInt(opcodeFragment[2])]);
+                if (opcodeFragment.size() > 2) {
+                    result = intcodeList.get(opcodeFragment.get(1)) + intcodeList.get(opcodeFragment.get(2));
                 }
                 break;
             case 2:
-                if (opcodeFragment[1] != null && opcodeFragment[2] != null) {
-                    result = Integer.parseInt(intcodeArray[Integer.parseInt(opcodeFragment[1])]) * Integer.parseInt(intcodeArray[Integer.parseInt(opcodeFragment[2])]);
+                if (opcodeFragment.size() > 2) {
+                    result = intcodeList.get(opcodeFragment.get(1)) * intcodeList.get(opcodeFragment.get(2));
                 }
                 break;
             case 99:
@@ -46,8 +49,8 @@ public class Day2Test1 {
                 throw new IllegalStateException("Unexpected value: " + opcode);
         }
         List<Integer> positionValueList = new ArrayList<>();
-        if (opcodeFragment[3] != null) {
-            positionValueList.add(Integer.parseInt(opcodeFragment[3]));
+        if (opcodeFragment.size() > 3) {
+            positionValueList.add(opcodeFragment.get(3));
         } else {
             positionValueList.add(-1);
         }
