b521b2a9d1b41ac2d17f39570e39e249358805cd
Sébastien DEKEYSER <sdekeyser@oppido.fr>
2019-12-09 12:59:04 +0100

    refactor project

--- src/main/java/fr/sebdek/adventofcode/Day2Test2.java
+++ src/main/java/fr/sebdek/adventofcode/Day2Test2.java
@@ -5,7 +5,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
