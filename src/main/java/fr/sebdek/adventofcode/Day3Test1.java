782f76dbc0078ca1bc06b6c3ed6c64813b7438ca
Sébastien DEKEYSER <sdekeyser@oppido.fr>
2019-12-10 16:24:57 +0100

    day3test1 fin

--- src/main/java/fr/sebdek/adventofcode/Day3Test1.java
+++ src/main/java/fr/sebdek/adventofcode/Day3Test1.java
@@ -5,47 +5,58 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day3Test1 {
 
-    private static int result = 19690720;
-
     public static void main(String[] args) throws IOException {
-        String line1 = "R8,U5,L5,D3";
-        String line2 = "U7,R6,D4,L4";
-        Map<Integer, Integer> positions1 = new HashMap<>();
-        Map<Integer, Integer> positions2 = new HashMap<>();
-        positions1.put(0, 0);
-        positions2.put(0, 0);
-        Files.lines(Paths.get("day3_test1.txt"));
+        try (Stream<String> lines = Files.lines(Paths.get("day3_test1.txt"))) {
+            List<Wire> wires = lines
+                    .map(Wire::new)
+                    .collect(Collectors.toList());
+            Map<Integer, List<Position>> positions1 = generatePositions(wires.get(0));
+            Map<Integer, List<Position>> positions2 = generatePositions(wires.get(1));
+            Position closestIntersection = findClosestPosition(positions1, positions2);
+            System.out.println(closestIntersection);
+        }
     }
 
-    //TODO
-    public Position findClosestPosition(Map<Integer, List<Position>> positions1, Map<Integer, List<Position>> positions2) {
-        positions1.keySet().stream();
-        return null;
+    public static Position findClosestPosition(Map<Integer, List<Position>> positions1, Map<Integer, List<Position>> positions2) {
+        Optional<Position> closestPosition = positions1.entrySet().stream()
+                .map(entry -> {
+                    Position closestCommonPoint = null;
+                    int currentDistance = entry.getKey();
+                    if (positions2.get(currentDistance) != null) {
+                        Optional<Position> onePosition = positions2.get(currentDistance).stream()
+                                .filter(position -> entry.getValue().indexOf(position) != -1)
+                                .findFirst();
+                        if (onePosition.isPresent()) {
+                            closestCommonPoint = onePosition.get();
+                        }
+                    }
+                    return closestCommonPoint;
+                })
+                .filter(Objects::nonNull)
+                .min(Comparator.comparingInt(Position::getDistance));
+        return closestPosition.orElse(null);
     }
 
     public static class Wire {
 
-        Set<Movement> movements;
+        List<Movement> movements;
 
         public Wire(String path) {
             this.movements = Arrays.stream(path.split(","))
-                    .map(s -> {
-                        String[] movementDesc = s.split("");
-                        return new Movement(movementDesc[0], Integer.parseInt(movementDesc[1]));
-                    })
-                    .collect(Collectors.toSet());
+                    .map(s -> new Movement(s.substring(0, 1), Integer.parseInt(s.substring(1))))
+                    .collect(Collectors.toList());
         }
 
-        public Set<Movement> getMovements() {
+        public List<Movement> getMovements() {
             return movements;
         }
 
-        public void setMovements(Set<Movement> movements) {
+        public void setMovements(List<Movement> movements) {
             this.movements = movements;
         }
     }
@@ -59,16 +70,8 @@ public class Day3Test1 {
             return direction;
         }
 
-        public void setDirection(String direction) {
-            this.direction = direction;
-        }
-
         public int getValue() {
             return value;
-        }
-
-        public void setValue(int value) {
-            this.value = value;
         }
 
         public Movement(String direction, int value) {
@@ -81,11 +84,11 @@ public class Day3Test1 {
                     break;
                 case "L":
                     this.direction = "R";
-                    this.value = - value;
+                    this.value = -value;
                     break;
                 case "D":
                     this.direction = "U";
-                    this.value = - value;
+                    this.value = -value;
                     break;
                 default:
                     break;
@@ -95,7 +98,7 @@ public class Day3Test1 {
 
     }
 
-    public class Position {
+    public static class Position {
 
         int x;
         int y;
@@ -105,24 +108,12 @@ public class Day3Test1 {
             return x;
         }
 
-        public void setX(int x) {
-            this.x = x;
-        }
-
         public int getY() {
             return y;
         }
 
-        public void setY(int y) {
-            this.y = y;
-        }
-
         public int getDistance() {
             return distance;
-        }
-
-        public void setDistance(int distance) {
-            this.distance = distance;
         }
 
         public Position(Position previousPosition, Movement movement) {
@@ -134,7 +125,7 @@ public class Day3Test1 {
                     this.x = 0;
                     this.y = movement.getValue();
                 }
-                this.distance = x + y;
+                this.distance = Math.abs(x) +  Math.abs(y);
             } else {
                 if ("R".equals(movement.getDirection())) {
                     this.x = previousPosition.getX() + movement.getValue();
@@ -143,20 +134,46 @@ public class Day3Test1 {
                     this.x = previousPosition.getX();
                     this.y = previousPosition.getY() + movement.getValue();
                 }
-                this.distance = previousPosition.getDistance() + x + y;
+                this.distance = Math.abs(x) +  Math.abs(y);
             }
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Position) {
+                return ((Position) obj).getX() == this.x && ((Position) obj).getY() == this.y && ((Position) obj).getDistance() == this.distance;
+            } else {
+                return super.equals(obj);
+            }
+        }
+
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return "position : x=" + x + ", y=" + y + ", distance=" + distance;
         }
     }
 
-    public Map<Integer, List<Position>> generatePositions(Wire wire) {
+    public static Map<Integer, List<Position>> generatePositions(Wire wire) {
         AtomicReference<Position> previousPosition = new AtomicReference<>();
         return wire.getMovements().stream()
-                .map(movement -> {
-                    Position newPosition = new Position(previousPosition.get(), movement);
-                    previousPosition.set(newPosition);
-                    return newPosition;
-                })
+                .map(movement -> generatePositionsFromMovement(previousPosition, movement))
+                .flatMap(List::stream)
                 .collect(Collectors.groupingBy(Position::getDistance));
+    }
+
+    private static List<Position> generatePositionsFromMovement(AtomicReference<Position> previousPosition, Movement movement) {
+        List<Position> positions = new ArrayList<>();
+        int absoluteValue = Math.abs(movement.getValue());
+        for (int i = 0; i < absoluteValue; i++) {
+            previousPosition.set(new Position(previousPosition.get(), new Movement(movement.getDirection(), movement.getValue() > 0 ? 1 : -1)));
+            positions.add(previousPosition.get());
+        }
+        return positions;
     }
 
 }
--- src/test/java/fr/sebdek/adventofcode/test/Day3Test1Test.java
+++ src/test/java/fr/sebdek/adventofcode/test/Day3Test1Test.java
@@ -3,29 +3,41 @@ package fr.sebdek.adventofcode.test;
 import fr.sebdek.adventofcode.Day3Test1;
 import fr.sebdek.adventofcode.Day3Test1.Position;
 import fr.sebdek.adventofcode.Day3Test1.Wire;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class Day3Test1Test {
-
-    private Day3Test1 testInstance = new Day3Test1();
-
-    @Before
-    public void init() {
-
-    }
 
     @Test
     public void simpleDataTest() {
         Wire wire1 = new Wire( "R8,U5,L5,D3");
         Wire wire2 = new Wire("U7,R6,D4,L4");
-        Map<Integer, List<Position>> positions1 = testInstance.generatePositions(wire1);
-        Map<Integer, List<Position>> positions2 = testInstance.generatePositions(wire2);
-        Position closestIntersection = testInstance.findClosestPosition(positions1, positions2);
+        Position closestIntersection = getPosition(wire1, wire2);
+        assert closestIntersection.getDistance() == 6;
+    }
+
+    @Test
+    public void dataTest1() {
+        Wire wire1 = new Wire( "R75,D30,R83,U83,L12,D49,R71,U7,L72");
+        Wire wire2 = new Wire("U62,R66,U55,R34,D71,R55,D58,R83");
+        Position closestIntersection = getPosition(wire1, wire2);
+        assert closestIntersection.getDistance() == 159;
+    }
+
+    @Test
+    public void dataTest2() {
+        Wire wire1 = new Wire( "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51");
+        Wire wire2 = new Wire("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7");
+        Position closestIntersection = getPosition(wire1, wire2);
+        assert closestIntersection.getDistance() == 135;
+    }
+
+    private Position getPosition(Wire wire1, Wire wire2) {
+        Map<Integer, List<Position>> positions1 = Day3Test1.generatePositions(wire1);
+        Map<Integer, List<Position>> positions2 = Day3Test1.generatePositions(wire2);
+        return Day3Test1.findClosestPosition(positions1, positions2);
     }
 
 }
