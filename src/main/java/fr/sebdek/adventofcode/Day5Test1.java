fca6bf53589ba67c09952dabb60d01b99cfaf3c6
Sébastien DEKEYSER <sdekeyser@oppido.fr>
2019-12-19 14:20:12 +0100

    day5test1 fin

--- src/main/java/fr/sebdek/adventofcode/Day5Test1.java
+++ src/main/java/fr/sebdek/adventofcode/Day5Test1.java
@@ -17,6 +17,7 @@ public class Day5Test1 {
             List<Integer> outputList = new ArrayList<>();
             List<Integer> intcodeList = processIntcode(intcode, input, outputList);
             System.out.println(intcodeList);
+            System.out.println(outputList);
         }
     }
 
@@ -29,70 +30,82 @@ public class Day5Test1 {
             int fragmentLength = 0;
             List<Integer> opcodeFragment = new ArrayList<>();
             int instruction = intcodeList.get(opcodePosition);
-            int donePart = Math.floorDiv(instruction, (int) Math.pow(10, 4));
-            for (int j = 3; j >= 0; j --) {
-                int currentDigit = Math.floorDiv(instruction, (int) Math.pow(10, j)) - donePart *10;
-                if (donePart > 0 || j <= 1) {
-                    opcodeFragment.add(currentDigit);
-                    fragmentLength ++;
+            if (instruction == 99) {
+                fragmentLength = 1;
+                opcodeFragment.add(instruction);
+            } else {
+                int donePart = Math.floorDiv(instruction, (int) Math.pow(10, 4));
+                for (int j = 3; j >= 0; j--) {
+                    int currentDigit = Math.floorDiv(instruction, (int) Math.pow(10, j)) - donePart * 10;
+                    if (donePart > 0 || (donePart == 0 && currentDigit != 0)) {
+                        opcodeFragment.add(currentDigit);
+                        fragmentLength++;
+                    }
+                    donePart = donePart * 10 + currentDigit;
                 }
-                donePart = donePart * 10 + currentDigit;
+            }
+            int lastIndex = 0;
+            int opcode = opcodeFragment.get(fragmentLength - 1);
+            if (opcode == 1 || opcode == 2) {
+                lastIndex = opcodePosition + 4;
+            } else if (instruction == 99) {
+                lastIndex = opcodePosition + 1;
+            } else if (opcode == 3 || opcode == 4) {
+                lastIndex = opcodePosition + 2;
             }
             List<Integer> positionValueList = processOpcodeFragment(intcodeList,
-                    intcodeList.subList(opcodePosition, opcodePosition + fragmentLength), fragmentLength, opcodeFragment, input);
+                    intcodeList.subList(opcodePosition, lastIndex), fragmentLength, opcodeFragment, input, opcode);
             if (positionValueList.get(1) == -1) {
                 opcodePosition = intcodeList.size();
             } else {
-                if (positionValueList.get(0) != 0) {
-                    intcodeList.set(positionValueList.get(0), positionValueList.get(1));
-                } else {
+                if (positionValueList.get(0) == -1) {
                     outputList.add(positionValueList.get(1));
+                } else {
+                    intcodeList.set(positionValueList.get(0), positionValueList.get(1));
                 }
+                opcodePosition = lastIndex;
             }
-            opcodePosition = opcodePosition + fragmentLength;
         }
         return intcodeList;
     }
 
-    protected static List<Integer> processOpcodeFragment(List<Integer> intcodeList, List<Integer> data, int fragmentLength, List<Integer> instructions, int input) {
-        int position = getPositionFromData(data, fragmentLength);
-        int result = getResultFromOpcode(intcodeList, data, instructions, fragmentLength, input);
+    protected static List<Integer> processOpcodeFragment(List<Integer> intcodeList, List<Integer> data, int fragmentLength, List<Integer> instructions, int input, int opcode) {
+        int position = getPositionFromData(data, opcode);
+        int result = getResultFromOpcode(intcodeList, data, instructions, fragmentLength, input, opcode);
         List<Integer> positionValueList = new ArrayList<>();
         positionValueList.add(position);
         positionValueList.add(result);
         return positionValueList;
     }
 
-    private static int getPositionFromData(List<Integer> data, int fragmentLength) {
-        if (fragmentLength >= 4) {
+    private static int getPositionFromData(List<Integer> data, int opcode) {
+        if (opcode == 1 || opcode == 2) {
             return data.get(3);
-        } else if (fragmentLength == 2) {
+        } else if (opcode == 3) {
             return data.get(1);
         }
-        return 0;
+        return -1;
     }
 
-    private static int getResultFromOpcode(List<Integer> intcodeList, List<Integer> data, List<Integer> instructions, int fragmentLength, int input) {
-        int result = 0;
-        int opcode = instructions.get(fragmentLength - 1);
+    private static int getResultFromOpcode(List<Integer> intcodeList, List<Integer> data, List<Integer> instructions, int fragmentLength, int input, int opcode) {
+        int result;
+        if (fragmentLength >= 2) {
+            opcode += instructions.get(fragmentLength - 2) * 10;
+        }
         switch (opcode) {
             case 1:
-                if (fragmentLength >= 4) {
-                    result = getParameter(intcodeList, data, instructions, fragmentLength, 1)
-                            + getParameter(intcodeList, data, instructions, fragmentLength, 2);
-                }
+                result = getParameter(intcodeList, data, instructions, 1)
+                        + getParameter(intcodeList, data, instructions, 2);
                 break;
             case 2:
-                if (fragmentLength >= 4) {
-                    result = getParameter(intcodeList, data, instructions, fragmentLength, 1)
-                            * getParameter(intcodeList, data, instructions, fragmentLength, 2);
-                }
+                result = getParameter(intcodeList, data, instructions, 1)
+                        * getParameter(intcodeList, data, instructions, 2);
                 break;
             case 3:
                 result = input;
                 break;
             case 4:
-                result = intcodeList.get(data.get(1));
+                result = getParameter(intcodeList, data, instructions, 1);
                 break;
             case 99:
                 result = -1;
@@ -103,13 +116,17 @@ public class Day5Test1 {
         return result;
     }
 
-    private static Integer getParameter(List<Integer> intcodeList, List<Integer> data, List<Integer> instructions, int fragmentLength, int parameterNumber) {
-        if (instructions.get(fragmentLength - (parameterNumber + 1)) == 0) {
+    private static Integer getParameter(List<Integer> intcodeList, List<Integer> data, List<Integer> instructions, int parameterNumber) {
+        int position = instructions.size() - parameterNumber - 2;
+        if (instructions.size() > 1) {
+            if (position < 0 || instructions.get(position) == 0) {
+                return intcodeList.get(data.get(parameterNumber));
+            } else {
+                return data.get(parameterNumber);
+            }
+        } else {
             return intcodeList.get(data.get(parameterNumber));
-        } else if (instructions.get(fragmentLength - (parameterNumber + 1)) == 1) {
-            return data.get(parameterNumber);
         }
-        return 0;
     }
 
 }
--- src/test/java/fr/sebdek/adventofcode/test/Day5Test1Test.java
+++ src/test/java/fr/sebdek/adventofcode/test/Day5Test1Test.java
@@ -13,11 +13,36 @@ public class Day5Test1Test {
     @Test
     public void dataTest1() {
         String intcode = "3,0,4,0,99";
-        int input = 3;
+        int input = 10;
         List<Integer> outputList = new ArrayList<>();
         List<Integer> intcodeList = processIntcode(intcode, input, outputList);
         System.out.println(intcodeList);
+        System.out.println(outputList);
         assert outputList.get(0) == input;
+    }
+
+    @Test
+    public void dataTest2() {
+        String intcode = "1002,4,3,4,33";
+        int input = 10;
+        List<Integer> outputList = new ArrayList<>();
+        List<Integer> intcodeList = processIntcode(intcode, input, outputList);
+        System.out.println(intcodeList);
+        System.out.println(outputList);
+        assert outputList.size() == 0;
+        assert intcodeList.get(4) == 99;
+    }
+
+    @Test
+    public void dataTest3() {
+        String intcode = "1101,100,-1,4,0";
+        int input = 10;
+        List<Integer> outputList = new ArrayList<>();
+        List<Integer> intcodeList = processIntcode(intcode, input, outputList);
+        System.out.println(intcodeList);
+        System.out.println(outputList);
+        assert outputList.size() == 0;
+        assert intcodeList.get(4) == 99;
     }
 
 }
